ceda0d139cf668a2cb5d7d8be7004cb51fa727d3
benroth012 <benroth012@gmail.com>
2023-02-22 15:13:30 -0500

    Login Screen functionality added
    Changed the intent filter to contain account activity. Fixed gradle build error

--- app/src/main/java/com/example/coffeetrail/AccountActivity.java
+++ app/src/main/java/com/example/coffeetrail/AccountActivity.java
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class AccountActivity extends AppCompatActivity {
 
+    private static final String TAG = "AccountActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +24,35 @@ public class AccountActivity extends AppCompatActivity {
                     .add(R.id.fragment_container, fragment)
                     .commit();
         }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
     }
 }
