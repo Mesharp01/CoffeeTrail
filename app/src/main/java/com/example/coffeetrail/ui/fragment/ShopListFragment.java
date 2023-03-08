cfb1e101334c98f9a30421306f398ecff4f83c30
Hannah Nguyen <hannahnguyen@hannahs-air.lan>
2023-03-08 16:21:14 -0500

    added recycler view to shop list (not working)

--- app/src/main/java/com/example/coffeetrail/ui/fragment/ShopListFragment.java
+++ app/src/main/java/com/example/coffeetrail/ui/fragment/ShopListFragment.java
@@ -7,21 +7,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.coffeetrail.R;
 import com.example.coffeetrail.databinding.FragmentShopListBinding;
 import com.example.coffeetrail.model.CoffeeShop;
 import com.example.coffeetrail.model.CoffeeShopViewModel;
+import com.example.coffeetrail.model.ShopOrder;
+import com.example.coffeetrail.model.ShopOrderViewModel;
 import com.example.coffeetrail.model.UserAccountViewModel;
+
+import java.util.List;
 
 public class ShopListFragment extends Fragment {
     private CoffeeShopViewModel mShopViewModel;
+    private RecyclerView mShopRecyclerView;
+    private List<CoffeeShop> mShopList;
+    private ShopListFragment.ShopAdapter mShopAdapter;
+
+    private LiveData<List<CoffeeShop>> mShopLiveData = new MutableLiveData<>();
+
     private Button mVisitButton;
-    private FragmentShopListBinding binding;
+    //private FragmentShopListBinding binding;
     String[] shopArray = {"Starbucks","Dunkin","Kafe Kerouac","Sweetwaters",
             "WebOS","Ubuntu","Windows7","Max OS X"};
 
@@ -32,17 +49,44 @@ public class ShopListFragment extends Fragment {
         mShopViewModel = new ViewModelProvider((ViewModelStoreOwner) activity).get(CoffeeShopViewModel.class);
         fillCoffeeShopTable();
     }
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        binding = FragmentShopListBinding.inflate(inflater, container, false);
+//        View v = binding.getRoot();
+//        return v;
+//    }
+//
+//    public void onDestroyView(){
+//        super.onDestroyView();
+//        binding = null;
+//    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        binding = FragmentShopListBinding.inflate(inflater, container, false);
-        View v = binding.getRoot();
+        View v = inflater.inflate(R.layout.fragment_shop_recycler_view, container, false);
         return v;
     }
 
-    public void onDestroyView(){
-        super.onDestroyView();
-        binding = null;
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Activity activity = requireActivity();
+        mShopRecyclerView = view.findViewById(R.id.shop_recycler_view);
+        mShopRecyclerView.setLayoutManager(new LinearLayoutManager(activity));
+        showShops();
+    }
+
+    private void showShops() {
+
+        mShopLiveData = mShopViewModel.getAllCoffeeShops();
+        mShopList = mShopLiveData.getValue();
+        if (mShopList != null) {
+            mShopAdapter = new ShopListFragment.ShopAdapter(mShopList);
+            mShopRecyclerView.setAdapter(mShopAdapter);
+            mShopRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        }
     }
 
     private void fillCoffeeShopTable(){
@@ -51,4 +95,47 @@ public class ShopListFragment extends Fragment {
         mShopViewModel.insert(c1);
 
     }
+
+    private static class ShopHolder extends RecyclerView.ViewHolder {
+        private final TextView mShopTextView;
+
+        ShopHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_shop, parent, false));
+
+            mShopTextView = itemView.findViewById(R.id.shop_info);
+        }
+
+        void bind(CoffeeShop shop) {
+            String name = shop.getName();
+            mShopTextView.setText(name);
+        }
+    }
+
+    private class ShopAdapter extends RecyclerView.Adapter<ShopListFragment.ShopHolder> {
+
+        private final List<CoffeeShop> mShopList;
+
+        ShopAdapter(List<CoffeeShop> shopList) {
+            mShopList = shopList;
+        }
+
+        @NonNull
+        @Override
+        public ShopListFragment.ShopHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = requireActivity().getLayoutInflater();
+            return new ShopListFragment.ShopHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ShopListFragment.ShopHolder holder, int position) {
+            CoffeeShop shop = mShopList.get(position);
+            holder.bind(shop);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mShopList.size();
+        }
+    }
 }
+
