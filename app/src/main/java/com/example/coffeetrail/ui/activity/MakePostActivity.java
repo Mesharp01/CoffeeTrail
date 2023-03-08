a4853c15c79f00d05f299f11053dd5180078ff00
Mesharp01 <54689768+Mesharp01@users.noreply.github.com>
2023-03-08 15:15:22 -0500

    Added make post activity

---
+++ app/src/main/java/com/example/coffeetrail/ui/activity/MakePostActivity.java
@@ -0,0 +1,60 @@
+package com.example.coffeetrail.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.example.coffeetrail.R;
+import com.example.coffeetrail.ui.fragment.MakePostFragment;
+import com.example.coffeetrail.ui.fragment.ShopOrderFragment;
+
+public class MakePostActivity extends AppCompatActivity {
+    private static final String TAG = "MakePostActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_post);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container_view);
+        if(fragment == null){
+            fragment = new MakePostFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container_view, fragment)
+                    .commit();
+
+        }
+        Log.d(TAG, "onCreate() called");
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+}
