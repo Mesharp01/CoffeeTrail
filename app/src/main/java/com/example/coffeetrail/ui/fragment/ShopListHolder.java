62c12c8f6dcd80656daca9606e21427902f8ffe5
henguyen2110 <nguyen.2110@osu.edu>
2023-03-23 22:08:40 -0400

    working about button for shops

--- app/src/main/java/com/example/coffeetrail/ui/fragment/ShopListHolder.java
+++ app/src/main/java/com/example/coffeetrail/ui/fragment/ShopListHolder.java
@@ -1,12 +1,15 @@
 package com.example.coffeetrail.ui.fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -18,6 +21,7 @@ import com.example.coffeetrail.model.UserAccount;
 
 public class ShopListHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private final TextView mShopListTextView;
+    private Button mAboutButton;
     private Button mVisitButton;
     private Button mViewOrdersButton;
 
@@ -34,7 +38,9 @@ public class ShopListHolder extends RecyclerView.ViewHolder implements View.OnCl
         super(itemView);
         mShopListTextView = itemView.findViewById(R.id.list_item_shoplist);
         mViewOrdersButton = itemView.findViewById(R.id.see_orders_button);
+        mAboutButton = itemView.findViewById(R.id.about_button);
         mVisitButton = itemView.findViewById(R.id.visit_shop_button);
+        mAboutButton.setOnClickListener(this);
         mVisitButton.setOnClickListener(this);
         mViewOrdersButton.setOnClickListener(this);
         currentUser = user;
@@ -54,7 +60,18 @@ public class ShopListHolder extends RecyclerView.ViewHolder implements View.OnCl
     @Override
     public void onClick(View view) {
         final int viewId = view.getId();
-        if (viewId == R.id.visit_shop_button) {
+        if(viewId == R.id.about_button){
+            AppCompatActivity activity = (AppCompatActivity) view.getContext();
+            AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+            builder.setMessage(currentStore.getLocation() + "\n" + currentStore.getUrl());
+            builder.setTitle(currentStore.getName());
+            builder.setCancelable(false);
+            builder.setNegativeButton("Close", (DialogInterface.OnClickListener) (dialog, which) -> {
+                dialog.cancel();
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        }else if (viewId == R.id.visit_shop_button) {
             String storeName = mShopListTextView.getText().toString();
 
             Bundle bundle = new Bundle();
