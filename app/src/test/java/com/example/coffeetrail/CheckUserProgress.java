2b2e6860e87da3bc395bbd1946090ca6f4bfb506
benroth012 <benroth012@gmail.com>
2023-04-10 11:34:25 -0400

    Unit test code

---
+++ app/src/test/java/com/example/coffeetrail/CheckUserProgress.java
@@ -0,0 +1,14 @@
+package com.example.coffeetrail;
+
+import static org.junit.Assert.assertEquals;
+
+import com.example.coffeetrail.ui.fragment.MapsFragment;
+
+import org.junit.Test;
+
+public class CheckUserProgress {
+    @Test
+    public void userHasCorrectProgress() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ app/src/test/java/com/example/coffeetrail/checkUserAccount.java
@@ -0,0 +1,19 @@
+package com.example.coffeetrail;
+
+import static org.junit.Assert.assertEquals;
+
+
+import com.example.coffeetrail.model.UserAccountViewModel;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class checkUserAccount {
+    @Test
+    public void timeStampToDate() {
+        UserAccountViewModel userAccounts = new UserAccountViewModel();
+        userAccounts.
+        assertEquals(4, 2 + 2);
+    }
+}
